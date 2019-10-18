f392a4e31ff4e9964d1b84e5ca86cd642d81e99e
Dmytro Kalpakchi <dmytroka@kth.se>
2019-10-18 12:28:21 +0200

    Rescrape migrationsverket

--- scrapers/migrationsverket/scraper.py
+++ scrapers/migrationsverket/scraper.py
@@ -2,7 +2,7 @@ import json
 import time
 import uuid
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 
 
 addresses = json.load(open('migrationsverket_urls'))['urls']
@@ -16,10 +16,14 @@ def get_text_from_printable(html_text):
         for child in div:
             if child.name in ['h1', 'p']:
                 for tag in child:
-                    if tag.name != 'a':
+                    if type(tag) == NavigableString:
                         text += child.text
                         text += '\n\n'
-                        break
+                    else:
+                        has_class = tag.has_attr('class')
+                        if tag.name != 'a' and ((has_class and 'circle-number' not in tag['class']) or not has_class):
+                            text += child.text
+                            text += '\n\n'
             elif child.name in ['ul']:
                 text = text[:-1] # remove \n\n
                 for tag in child:
