ce012aef25b9eb7d2e46dab0479085d2ce4d3a1c
Dmytro Kalpakchi <dmytroka@kth.se>
2019-10-18 14:39:35 +0200

    Add h3 headers to the scraper

--- scrapers/migrationsverket/scraper.py
+++ scrapers/migrationsverket/scraper.py
@@ -16,33 +16,32 @@ def get_text_from_printable(html_text):
     header_start = 0 # where a new header have started
     for div in soup.find_all('div', {'class': 'sv-text-portlet-content'}):
         for child in div:
-            if child.name in ['h1', 'h2', 'p']:
+            if child.name in ['h1', 'h2', 'h3', 'p']:
+                if child.name == 'h1':
+                    header_start = len(text)
+                    text += '# '
+                elif child.name == 'h2':
+                    header_start = len(text)
+                    text += '## '
+
                 for tag in child:
                     num_elements += 1
-                    if type(tag) == NavigableString:
-                        if child.name == 'h1':
-                            header_start = len(text)
-                            text += f'# {tag}'
-                        elif child.name == 'h2':
-                            header_start = len(text)
-                            text += f'## {tag}'
-                        else:
-                            text += tag
+                    if type(tag) == NavigableString:                        
+                        text += tag.strip()
                     else:
                         if tag.name == 'a':
                             if num_elements > 1:
                                 text += link_content
-                                text += tag.text
+                                text += tag.text.strip()
                                 link_content = ""
                             else:
-                                link_content = tag.text
+                                link_content = tag.text.strip()
                         else:
                             has_class = tag.has_attr('class')
                             if (has_class and 'circle-number' not in tag['class']) or not has_class:
                                 text += tag.text
                 num_elements, link_content = 0, ""
             elif child.name in ['ul']:
-                text = text[:-1] # remove \n\n
                 for tag in child:
                     text += u"\u2022"
                     text += f" {tag.text}\n"
@@ -50,8 +49,10 @@ def get_text_from_printable(html_text):
             text += "\n\n"
 
             if len(text.split()) > 400:
-                parts.append(text[:header_start].strip())
-                text = text[header_start:]
+                if header_start > 0:
+                    parts.append(text[:header_start].strip())
+                    text = text[header_start:]
+                header_start = 0
 
     if len(text.split()) < 100 and len(parts) > 0:
         parts[-1] += text
