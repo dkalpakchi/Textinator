f58536b7ca35684609c67dd84f95537a38d43346
Dmytro Kalpakchi <dmytroka@kth.se>
2019-10-18 14:21:38 +0200

    Rescrape Migrationsverket with dividing text into parts

--- scrapers/migrationsverket/scraper.py
+++ scrapers/migrationsverket/scraper.py
@@ -11,27 +11,54 @@ addresses = json.load(open('migrationsverket_urls'))['urls']
 def get_text_from_printable(html_text):
     soup = BeautifulSoup(html_text, 'html.parser')
 
-    text = ""
+    parts = []
+    text, num_elements, link_content = "", 0, ""
+    header_start = 0 # where a new header have started
     for div in soup.find_all('div', {'class': 'sv-text-portlet-content'}):
         for child in div:
-            if child.name in ['h1', 'p']:
+            if child.name in ['h1', 'h2', 'p']:
                 for tag in child:
+                    num_elements += 1
                     if type(tag) == NavigableString:
-                        text += child.text
-                        text += '\n\n'
+                        if child.name == 'h1':
+                            header_start = len(text)
+                            text += f'# {tag}'
+                        elif child.name == 'h2':
+                            header_start = len(text)
+                            text += f'## {tag}'
+                        else:
+                            text += tag
                     else:
-                        has_class = tag.has_attr('class')
-                        if tag.name != 'a' and ((has_class and 'circle-number' not in tag['class']) or not has_class):
-                            text += child.text
-                            text += '\n\n'
+                        if tag.name == 'a':
+                            if num_elements > 1:
+                                text += link_content
+                                text += tag.text
+                                link_content = ""
+                            else:
+                                link_content = tag.text
+                        else:
+                            has_class = tag.has_attr('class')
+                            if (has_class and 'circle-number' not in tag['class']) or not has_class:
+                                text += tag.text
+                num_elements, link_content = 0, ""
             elif child.name in ['ul']:
                 text = text[:-1] # remove \n\n
                 for tag in child:
                     text += u"\u2022"
                     text += f" {tag.text}\n"
-                text += "\n\n"
+            text = text.strip()
+            text += "\n\n"
 
-    return text.strip()
+            if len(text.split()) > 400:
+                parts.append(text[:header_start].strip())
+                text = text[header_start:]
+
+    if len(text.split()) < 100 and len(parts) > 0:
+        parts[-1] += text
+    else:
+        parts.append(text)
+
+    return parts
 
 def crawl(url):
     res = requests.get("{}.printable".format(url))
@@ -41,7 +68,9 @@ def crawl(url):
 
 if __name__ == '__main__':
     for url in addresses:
-        json.dump({
-            'url': url,
-            'text': get_text_from_printable(crawl(url))
-        }, open('migrationsverket_{}.json'.format(uuid.uuid4().hex), 'w'))
+        url_hex = uuid.uuid4().hex
+        for i, part in enumerate(get_text_from_printable(crawl(url))):
+            json.dump({
+                'url': url,
+                'text': part
+            }, open(f'migrationsverket_{url_hex}_p{i}.json', 'w'))
