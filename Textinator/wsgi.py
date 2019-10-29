f82a748f0d09683a6b894f23eb79d9b915719f78
Dmytro Kalpakchi <dmytroka@kth.se>
2019-10-29 16:30:32 +0100

    Load dotenv in wsgi

--- Textinator/wsgi.py
+++ Textinator/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import dotenv
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Textinator.settings')
+dotenv.load_dotenv()
 
 application = get_wsgi_application()
