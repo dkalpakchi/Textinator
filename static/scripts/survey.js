403c1f951527d85ffd37edcbb7b71b44d17352fc
Dmytro Kalpakchi <dmytroka@kth.se>
2020-10-05 13:23:30 +0200

    Add form validation to the survey

--- static/scripts/survey.js
+++ static/scripts/survey.js
@@ -3,4 +3,30 @@ $(document).ready(function() {
     collapsible: true,
     active: false
   });
+
+  $('form').validate({
+    errorClass: "error tag is-danger",
+    invalidHandler: function(event, validator) {
+      event.preventDefault();
+    },
+    errorPlacement: function(label, element) {
+      if (element.parent().prop('tagName') == 'LABEL') {
+        element.parent().parent().append(label);
+      } else {
+        element.parent().append(label);
+      }
+    },
+    highlight: function(element, errorClass, validClass) {
+      $(element).addClass(errorClass).removeClass(validClass).removeClass('tag is-danger');
+    },
+    unhighlight: function(element, errorClass, validClass) {
+      $(element).removeClass(errorClass).addClass(validClass);
+    }
+  });
+
+  $('input[type!="hidden"]').each(function() {
+    $(this).rules('add', {
+      required: true
+    });
+  });
 });
